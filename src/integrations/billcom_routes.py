e30851c43bae246cc9a4222e0418e594f66f43a4
Anthony <anthony@example.com>
2025-11-24 04:49:01 -0500

    Remove: Delete conflicting Flask src/ and wsgi.py from root

--- src/integrations/billcom_routes.py
+++
@@ -1,8 +0,0 @@
-"""Bill.com integration routes"""
-from flask import Blueprint
-
-billcom_bp = Blueprint('billcom', __name__, url_prefix='/api/billcom')
-
-@billcom_bp.route('/health', methods=['GET'])
-def health():
-    return {'status': 'ok'}
--- src/integrations/intuit_routes.py
+++
@@ -1,8 +0,0 @@
-"""Intuit integration routes"""
-from flask import Blueprint
-
-intuit_bp = Blueprint('intuit', __name__, url_prefix='/api/intuit')
-
-@intuit_bp.route('/health', methods=['GET'])
-def health():
-    return {'status': 'ok'}
